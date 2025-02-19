cac52f777e037de0cad933df727d5f158693f619
matasrasimas <matasra2002@gmail.com>
2025-02-19 22:39:12 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+const App = () => {
+    return (
+        <div className="flex items-center flex-col bg-gray-500 w-full">
+            <h2 className="text-red-500 text-[55px]">Hello world</h2>
+        </div>
+    );
+}
+
+export default App;
